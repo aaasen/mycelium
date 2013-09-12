89a37cf6aa67104fc11c11ec6d78137f03029368
Lane Aasen <laneaasen@gmail.com>
2013-09-11 21:04:49 -0700

    links are now parsed from pages in preparation for recursive crawling

--- crawl.go
+++ crawl.go
@@ -1,6 +1,7 @@
 package crawl
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -9,10 +10,26 @@ func Fetch(url string, pages chan Page) {
 	resp, err := http.Get(url)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Fetch(): %v", err)
 	}
 
 	page := NewPage(resp)
 
 	pages <- *page
+}
+
+func Crawl(urls []string, pages chan Page) {
+	for _, url := range urls {
+		fmt.Println("url")
+		go Fetch(url, pages)
+	}
+
+	for page := range pages {
+		fmt.Println(page.URL)
+		fmt.Println(page.links)
+
+		for _, link := range page.links {
+			go Fetch(link.String(), pages)
+		}
+	}
 }
--- page.go
+++ page.go
@@ -1,27 +1,49 @@
 package crawl
 
 import (
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 type Page struct {
-	URL     *url.URL
-	Content []byte
+	URL string
+
+	response *http.Response
+	links    []*url.URL
 }
 
 func NewPage(resp *http.Response) *Page {
-	content, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
+	return &Page{
+		URL:      resp.Request.URL.String(),
+		response: resp,
+		links:    getLinks(resp.Body),
+	}
+}
+
+func getLinks(content io.Reader) []*url.URL {
+	doc, err := goquery.NewDocumentFromReader(content)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return &Page{
-		URL:     resp.Request.URL,
-		Content: content,
-	}
+	urls := []*url.URL{}
+
+	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+		if href, hrefExists := s.Attr("href"); hrefExists {
+			url, err := url.Parse(href)
+
+			if err != nil {
+				log.Fatalf("GetLinks(): %v", err)
+			}
+
+			urls = append(urls, url)
+		}
+	})
+
+	return urls
 }
