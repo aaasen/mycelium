7e99ff66baf9bb092a4e4cd0930ace65aef0724c
Lane Aasen <laneaasen@gmail.com>
2013-09-13 13:29:32 -0700

    removes DebugDataStore

--- data_store.go
+++ data_store.go
@@ -1,34 +1,7 @@
 package mycelium
 
-import (
-	"log"
-)
+import ()
 
 type DataStore interface {
 	Save(*Page) error
-}
-
-type DebugDataStore struct {
-	Pages <-chan Page
-}
-
-func NewDebugDataStore(pages <-chan Page) *DebugDataStore {
-	return &DebugDataStore{
-		Pages: pages,
-	}
-}
-
-func (self *DebugDataStore) Run() {
-	for {
-		select {
-		case page := <-self.Pages:
-			self.Save(&page)
-		}
-	}
-}
-
-func (self *DebugDataStore) Save(page *Page) error {
-	log.Printf("received: %v", page.URL)
-
-	return nil
 }
